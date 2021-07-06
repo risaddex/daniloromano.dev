9fb9d63071d380791bc9162b22a86eb151653d00
risaddex <risaddex@gmail.com>
2021-07-06 12:49:24 -0300

    added headers to get repository tags

--- src/utils/fetchProjects.ts
+++ src/utils/fetchProjects.ts
@@ -1,19 +1,11 @@
-// https://api.github.com/users/risaddex/repos?sort=indexed
-// license SHAPE
-/* {
-  "key": "mit",
-  "name": "MIT License",
-  "spdx_id": "MIT",
-  "url": "https://api.github.com/licenses/mit",
-  "node_id": "MDc6TGljZW5zZTEz"
-}, */
+
 export type TGitHubRepo = {
   name: string;
   html_url: string;
   description: string;
   languages_url: string;
   clone_url: string;
-  tags_url:string;
+  tags_url: string;
   created_at: string;
   updated_at: string;
   homepage: string;
@@ -22,19 +14,23 @@ export type TGitHubRepo = {
   archived: boolean;
   disabled: boolean;
   fork: boolean;
+  topics: string[];
   stargazers_count: number;
-  license: Object;
+  license: Record<string, unknown>;
 };
 
 const GITHUB_USERNAME = 'risaddex';
 
-let sortMode = 'indexed';
-let page = 1;
+const sortMode = 'indexed';
+const page = 1;
 
-export async function fetchGitHubRepos():Promise<TGitHubRepo[]> {
+export async function fetchGitHubRepos(): Promise<TGitHubRepo[]> {
   const res = await fetch(
-    `https://api.github.com/users/${GITHUB_USERNAME}/repos?sort=${sortMode}&page=${page}`
+    `https://api.github.com/users/${GITHUB_USERNAME}/repos?sort=${sortMode}&page=${page}`,
+    // required in order to get the list of topics
+    { headers: { Accept: 'application/vnd.github.mercy-preview+json' } }
   );
-  const data = await res.json() as TGitHubRepo[];
-  return data;
+  const repositoryData = (await res.json()) as TGitHubRepo[];
+
+  return repositoryData;
 }
